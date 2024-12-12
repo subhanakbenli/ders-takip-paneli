b042be2276f811a6d3097398da6faeaf1a34fbc6
sübhan akbenli <96058973+subhanakbenli@users.noreply.github.com>
2024-12-12 16:19:36 +0300

    modeller oluşturludu

--- ders_takip/dersler/models.py
+++ ders_takip/dersler/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Ogretmen(models.Model):
+    ad_soyad = models.CharField(max_length=255, verbose_name="Ad Soyad")
+    telefon = models.CharField(max_length=255, verbose_name="Telefon")
+    email = models.EmailField(max_length=255, verbose_name="Email")
+    aciklama = models.TextField(verbose_name="Açıklama")
+    def __str__(self):
+        return self.ad_soyad
+
+class Ders(models.Model):
+    dersi_veren = models.ForeignKey(Ogretmen, on_delete=models.CASCADE)
+    ders_adi = models.CharField(max_length=255, verbose_name="Ders Adı")
+    durum = models.CharField(max_length=255, verbose_name="Durum")
+    aciklama = models.TextField(verbose_name="Açıklama")
+    def __str__(self):
+        return self.ders_adi
+    
+class DersBelgesi(models.Model):
+    ders = models.ForeignKey(Ders, on_delete=models.CASCADE)
+    kategori = models.CharField(max_length=255, verbose_name="Kategori")
+    belge_adi = models.CharField(max_length=255, verbose_name="Belge Adı")
+    belge = models.FileField(upload_to="ders_belgeleri/", verbose_name="Belge")
+    def __str__(self):
+        return f"{self.kategori} + {self.belge_adi}"
+
--- ders_takip/pages/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class DersBilgisi(models.Model):
-    ders_adi = models.CharField(max_length=255, verbose_name="Birisinin Vereceği Ders")
-    durum = models.CharField(max_length=255, verbose_name="Durum")
-    yukleme_miktari = models.PositiveIntegerField(verbose_name="Yükleme Miktarı", default=0)
-
-    def __str__(self):
-        return self.ders_adi
