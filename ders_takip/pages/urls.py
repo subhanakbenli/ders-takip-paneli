593acf7833beca0551ff6a3636d1e9b18c863ada
hamza-bugra <hamzabugraveske1@gmail.com>
2024-12-14 03:12:09 +0300

    ++

--- ders_takip/pages/urls.py
+++ ders_takip/pages/urls.py
@@ -4,5 +4,5 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("arsiv",views.arsiv_view,name = 'arsiv_view')
+    path("arsiv",views.arsiv_view,name = 'arsiv_view'),
 ]
