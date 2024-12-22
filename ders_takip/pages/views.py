c22b7dce9ceed25820355c2d8df09222a3446725
hamza-bugra <hamzabugraveske1@gmail.com>
2024-12-22 14:29:56 +0300

    arşiv sayfası
    Arşiv sayfası güncelleme

--- ders_takip/pages/views.py
+++ ders_takip/pages/views.py
@@ -11,3 +11,30 @@ def index(request):
     return render(request, 'base.html')
 def arsiv_view(request):
     return render(request, 'pages/arsiv.html')
+
+def arsiv_view(request):
+    teachers = [ "Sübhan Akbenli"]  
+    table_data = [
+        {
+            "proje": "Proje 1",
+            "panoya": {
+                "yukleme_alani": "Yükleme Alanı 1",
+                "etkinlik_tarihi": "2024-12-20",
+                "yukleme_tarihi": "2024-12-21",
+            },
+            "erp": {
+                "ERP Sisteme Yükleme Numarası":"21312",
+                "aciklama": "Panoya Açıklama 1",
+                "yukleme_numarasi": "123456",
+                "kodu": "KD01",
+                "not": 85,
+                "dilekce": "fsdfsfsd",
+            },
+        },
+        
+    ]
+    context = {
+        "teachers": teachers,
+        "table_data": table_data,
+    }
+    return render(request, "pages/arsiv.html", context)
