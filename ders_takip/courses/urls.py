8a8f3944a5ca69a2fad51febb2e6265d39af5ce1
hamza-bugra <hamzabugraveske1@gmail.com>
2024-12-25 18:06:47 +0300

    ++

--- ders_takip/courses/urls.py
+++ ders_takip/courses/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("ders_listesi", views.show_courses_list, name="show_courses_list"),
     path("<int:id>", views.show_course_detail,name='show_course_detail'),
     path("pano", views.show_teacher_with_courses_and_documents, name="show_teacher_with_courses_and_documents"),
+    path('pano_özet/', views.pano_ozet, name='pano_ozet'),
     path("deneme_arsiv", views.deneme_arsiv, name="deneme_arsiv"),
     
     path('ders_sil/<int:id>/', views.delete_course, name='delete_course'),
@@ -22,4 +23,5 @@ urlpatterns = [
     path('erp/', views.erp_page, name='erp_page'),
     path('kaydet/<int:id>/', views.kaydet, name='kaydet'),
     path('arsive_al/<int:id>/', views.arsive_al, name='arsive_al'),
+   
 ]
