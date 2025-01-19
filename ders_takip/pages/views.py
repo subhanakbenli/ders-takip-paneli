f8a16cb3a43a29faf5719b7c00dd30af3176fead
hamza-bugra <hamzabugraveske1@gmail.com>
2025-01-19 15:32:03 +0300

    sayfa uyarıları bitti

--- ders_takip/pages/views.py
+++ ders_takip/pages/views.py
@@ -19,6 +19,24 @@ def erp_view(request):
     selected_teacher_id = request.GET.get('teacher_id')
     selected_course_name = request.GET.get('course_name', None)
     teachers_data = get_teachers_with_courses_and_documents(status="aktif", page="erp")
+    # Belge kalan günlerini hesapla
+    current_date = datetime.now().date()
+    for teacher in teachers_data:
+        for course in teacher.get('courses', []):  # Her öğretmen için kurslar
+            for document in course.get('documents', []):  # Her kurs için belgeler
+                end_date = document.get('end_date')
+                if end_date:
+                    try:
+                        # end_date string olarak geliyorsa datetime.date'e dönüştür
+                        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                        # Kalan günleri hesapla
+                        remaining_days = (end_date - current_date).days
+                        document['remaining_days'] = remaining_days
+                    except ValueError:
+                        # Geçersiz bir tarih formatı varsa None olarak ayarla
+                        document['remaining_days'] = None
+                else:
+                    document['remaining_days'] = None  # Bitiş tarihi yoksa None olarak ayarla
 
     return render(request, 'pages/erp.html', {'teachers': teachers_data,
                                                 'set_of_courses': distinct_courses,
@@ -32,6 +50,24 @@ def erp_arsiv_view(request):
     selected_teacher_id = request.GET.get('teacher_id')
     selected_course_name = request.GET.get('course_name', None)
     teachers_data = get_teachers_with_courses_and_documents(status="arsiv", page="erp")
+    # Belge kalan günlerini hesapla
+    current_date = datetime.now().date()
+    for teacher in teachers_data:
+        for course in teacher.get('courses', []):  # Her öğretmen için kurslar
+            for document in course.get('documents', []):  # Her kurs için belgeler
+                end_date = document.get('end_date')
+                if end_date:
+                    try:
+                        # end_date string olarak geliyorsa datetime.date'e dönüştür
+                        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                        # Kalan günleri hesapla
+                        remaining_days = (end_date - current_date).days
+                        document['remaining_days'] = remaining_days
+                    except ValueError:
+                        # Geçersiz bir tarih formatı varsa None olarak ayarla
+                        document['remaining_days'] = None
+                else:
+                    document['remaining_days'] = None  # Bitiş tarihi yoksa None olarak ayarla
     return render(request, 'pages/erp_arsiv.html', {'teachers': teachers_data,
                                                 'set_of_courses': distinct_courses,
                                                 'selected_teacher_id': int(selected_teacher_id) if selected_teacher_id else None, 
@@ -44,12 +80,30 @@ def erp_iptal_view(request):
     selected_teacher_id = request.GET.get('teacher_id')
     selected_course_name = request.GET.get('course_name', None)
     teachers_data = get_teachers_with_courses_and_documents(status="iptal", page="erp")
+     # Belge kalan günlerini hesapla
+    current_date = datetime.now().date()
+    for teacher in teachers_data:
+        for course in teacher.get('courses', []):  # Her öğretmen için kurslar
+            for document in course.get('documents', []):  # Her kurs için belgeler
+                end_date = document.get('end_date')
+                if end_date:
+                    try:
+                        # end_date string olarak geliyorsa datetime.date'e dönüştür
+                        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                        # Kalan günleri hesapla
+                        remaining_days = (end_date - current_date).days
+                        document['remaining_days'] = remaining_days
+                    except ValueError:
+                        # Geçersiz bir tarih formatı varsa None olarak ayarla
+                        document['remaining_days'] = None
+                else:
+                    document['remaining_days'] = None  # Bitiş tarihi yoksa None olarak ayarla
     return render(request, 'pages/erp_iptal.html', {'teachers': teachers_data,
                                                 'set_of_courses': distinct_courses,
                                                 'selected_teacher_id': int(selected_teacher_id) if selected_teacher_id else None, 
                                                 'selected_course_name': selected_course_name})
 
-
+from datetime import datetime
 @login_required
 def pano_view(request, teacher_id=None):
     distinct_courses = Course.objects.values('name').distinct()
@@ -57,6 +111,24 @@ def pano_view(request, teacher_id=None):
     selected_teacher_id = request.GET.get('teacher_id', teacher_id)
     selected_course_name = request.GET.get('course_name', None)
     teachers_data = get_teachers_with_courses_and_documents(status="aktif", page="pano")    
+   # Belge kalan günlerini hesapla
+    current_date = datetime.now().date()
+    for teacher in teachers_data:
+        for course in teacher.get('courses', []):  # Her öğretmen için kurslar
+            for document in course.get('documents', []):  # Her kurs için belgeler
+                end_date = document.get('end_date')
+                if end_date:
+                    try:
+                        # end_date string olarak geliyorsa datetime.date'e dönüştür
+                        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                        # Kalan günleri hesapla
+                        remaining_days = (end_date - current_date).days
+                        document['remaining_days'] = remaining_days
+                    except ValueError:
+                        # Geçersiz bir tarih formatı varsa None olarak ayarla
+                        document['remaining_days'] = None
+                else:
+                    document['remaining_days'] = None  # Bitiş tarihi yoksa None olarak ayarla
     return render(request, 'pages/pano.html', {'teachers': teachers_data,
                                                 'set_of_courses': distinct_courses,
                                                 'selected_teacher_id': int(selected_teacher_id) if selected_teacher_id else None, 
@@ -69,6 +141,24 @@ def pano_arsiv_view(request,teacher_id=None):
     selected_teacher_id = request.GET.get('teacher_id', teacher_id)
     selected_course_name = request.GET.get('course_name', None)
     teachers_data = get_teachers_with_courses_and_documents(status="arsiv", page="pano")
+    # Belge kalan günlerini hesapla
+    current_date = datetime.now().date()
+    for teacher in teachers_data:
+        for course in teacher.get('courses', []):  # Her öğretmen için kurslar
+            for document in course.get('documents', []):  # Her kurs için belgeler
+                end_date = document.get('end_date')
+                if end_date:
+                    try:
+                        # end_date string olarak geliyorsa datetime.date'e dönüştür
+                        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                        # Kalan günleri hesapla
+                        remaining_days = (end_date - current_date).days
+                        document['remaining_days'] = remaining_days
+                    except ValueError:
+                        # Geçersiz bir tarih formatı varsa None olarak ayarla
+                        document['remaining_days'] = None
+                else:
+                    document['remaining_days'] = None  # Bitiş tarihi yoksa None olarak ayarla
     return render(request, 'pages/pano_arsiv.html', {'teachers': teachers_data,
                                                 'set_of_courses': distinct_courses,
                                                 'selected_teacher_id': int(selected_teacher_id) if selected_teacher_id else None, 
@@ -81,6 +171,24 @@ def pano_iptal_view(request,teacher_id=None):
     selected_teacher_id = request.GET.get('teacher_id', teacher_id)
     selected_course_name = request.GET.get('course_name', None)
     teachers_data = get_teachers_with_courses_and_documents(status="iptal", page="pano")
+    # Belge kalan günlerini hesapla
+    current_date = datetime.now().date()
+    for teacher in teachers_data:
+        for course in teacher.get('courses', []):  # Her öğretmen için kurslar
+            for document in course.get('documents', []):  # Her kurs için belgeler
+                end_date = document.get('end_date')
+                if end_date:
+                    try:
+                        # end_date string olarak geliyorsa datetime.date'e dönüştür
+                        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                        # Kalan günleri hesapla
+                        remaining_days = (end_date - current_date).days
+                        document['remaining_days'] = remaining_days
+                    except ValueError:
+                        # Geçersiz bir tarih formatı varsa None olarak ayarla
+                        document['remaining_days'] = None
+                else:
+                    document['remaining_days'] = None  # Bitiş tarihi yoksa None olarak ayarla
     return render(request, 'pages/pano_iptal.html', {'teachers': teachers_data,
                                                 'set_of_courses': distinct_courses,
                                                 'selected_teacher_id': int(selected_teacher_id) if selected_teacher_id else None, 
@@ -90,12 +198,46 @@ def pano_iptal_view(request,teacher_id=None):
 @login_required
 def pano_ozet_view(request):
     data = CourseFile.get_files_in_warning_period()
+    # Bugünün tarihini alın
+    current_date = datetime.now().date()
+
+    # Her belge için kalan günleri hesaplayın
+    for course_file in data:
+        end_date = course_file.end_date
+        if end_date:
+            try:
+                # Tarihi dönüştür ve kalan günleri hesapla
+                if isinstance(end_date, str):
+                    end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                remaining_days = (end_date - current_date).days
+                course_file.remaining_days = remaining_days
+            except ValueError:
+                course_file.remaining_days = None  # Geçersiz tarih formatı durumunda None ayarla
+        else:
+            course_file.remaining_days = None  # Bitiş tarihi yoksa None ayarla
     return render(request, 'pages/pano_ozet.html', {'data': data})
 
 
 @user_has_permission([SUPERUSER,ADMIN])
 def erp_ozet_view(request):
     data = CourseFile.get_files_in_warning_period()
+    # Bugünün tarihini alın
+    current_date = datetime.now().date()
+
+    # Her belge için kalan günleri hesaplayın
+    for course_file in data:
+        end_date = course_file.end_date
+        if end_date:
+            try:
+                # Tarihi dönüştür ve kalan günleri hesapla
+                if isinstance(end_date, str):
+                    end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+                remaining_days = (end_date - current_date).days
+                course_file.remaining_days = remaining_days
+            except ValueError:
+                course_file.remaining_days = None  # Geçersiz tarih formatı durumunda None ayarla
+        else:
+            course_file.remaining_days = None  # Bitiş tarihi yoksa None ayarla
     return render(request, 'pages/erp_ozet.html', {'data':data})
 
 
